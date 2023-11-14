1a2f0b2a7f3ead8078a97b563e90a2d2caaf49c8
joltzen <jason.oltzen@smail.th-koeln.de>
2023-11-14 01:20:12 +0100

    add different variables for dev and prod

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5432;
+const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const path = require("path");
 
--- server/routes/db.js
+++ server/routes/db.js
@@ -1,17 +1,26 @@
 const config = require("./config");
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  user: config.database.user,
-  host: config.database.host,
-  database: config.database.database,
-  password: config.database.password,
-  port: config.database.port,
-  // connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-});
-
-module.exports = pool;
+if (process.env.NODE_ENV == "production") {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    user: config.database.user,
+    host: config.database.host,
+    database: config.database.database,
+    password: config.database.password,
+    port: config.database.port,
+  });
+  module.exports = pool;
+} else {
+  const pool = new Pool({
+    user: config.database.user,
+    host: config.database.host,
+    database: config.database.database,
+    password: config.database.password,
+    port: config.database.port,
+  });
+  module.exports = pool;
+}
