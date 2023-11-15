8278434b5a98a7aff9d9a80ee2a6faf71ff067d1
joltzen <jason.oltzen@smail.th-koeln.de>
2023-11-15 14:05:27 +0100

    WIP Contact Page

--- client/src/pages/Contact/contact.js
+++ client/src/pages/Contact/contact.js
@@ -1,10 +1,67 @@
 import React from "react";
 import Page from "../../components/page";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Paper,
+} from "@mui/material";
 
 function ContactPage() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Formular gesendet");
+  };
+
   return (
     <Page>
-      <h1>Welcome to the Contact Page!</h1>
+      <Container maxWidth="md">
+        <Typography variant="h4" component="h1" gutterBottom>
+          Willkommen auf der Kontaktseite!
+        </Typography>
+        <Paper elevation={3} sx={{ padding: 4 }}>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { margin: 2, width: "100%" },
+              "& .MuiButton-root": { margin: 2 },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <Typography variant="h6" component="h2" gutterBottom>
+              Kontaktformular
+            </Typography>
+            <TextField required label="Name" variant="outlined" />
+            <TextField
+              required
+              label="E-Mail"
+              variant="outlined"
+              type="email"
+            />
+            <TextField
+              label="Nachricht"
+              variant="outlined"
+              multiline
+              rows={4}
+              required
+            />
+            <Button variant="contained" type="submit">
+              Senden
+            </Button>
+          </Box>
+        </Paper>
+        <Typography variant="h6" sx={{ mt: 4 }}>
+          Direkter Kontakt
+        </Typography>
+        <Typography variant="body1">
+          Sie erreichen uns auch direkt per E-Mail unter:
+          webmaster@financeforstudents.com
+        </Typography>
+      </Container>
     </Page>
   );
 }
