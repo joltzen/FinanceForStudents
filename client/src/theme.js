88b294e517342004d7bf71930808dad48929336a
joltzen <jason.oltzen@smail.th-koeln.de>
2023-11-24 20:04:32 +0100

    change theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -33,7 +33,7 @@ export const tokens = (mode) => {
           //not used
           brightYellow: "#ffce56",
           //budget.main
-          neonGreen: "#71C839",
+          neonGreen: "#65C023",
           //saving.main
           skyBlue: "#4dd0e1",
           //pagination.main
@@ -81,7 +81,7 @@ export const tokens = (mode) => {
           //not used
           brightYellow: "#FFD700",
           //budget.main
-          neonGreen: "#71C839",
+          neonGreen: "#65C023",
           //saving.main
           skyBlue: "#00BFFF",
           //pagination.main
